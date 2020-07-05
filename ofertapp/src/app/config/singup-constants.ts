89a8a8d3324a863701d24d7eb5b9644159b93f89
anahi <rsanahi@users.noreply.github.com>
2020-07-05 14:48:05 -0400

    Sing up slider

---
+++ ofertapp/src/app/config/singup-constants.ts
@@ -0,0 +1,17 @@
+export class SingupConstants{
+    public static readonly CLIENTE = {
+        USER_TYPE: 'Cliente',
+        FORM: {
+            primary_info: 'Informacion Personal',
+            user_info: 'Informacion de la Cuenta',
+        },
+    }
+
+    public static readonly EMPRESA = {
+        USER_TYPE: 'Empresa',
+        FORM: {
+            primary_info: 'Informacion de la Empresa',
+            user_info: 'Informacion de la Cuenta',
+        },
+    }
+}
--- ofertapp/src/app/register/register.page.ts
+++ ofertapp/src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { IonSlides} from '@ionic/angular';
+import { SingupConstants } from '../config/singup-constants';
+import { NONE_TYPE } from '@angular/compiler';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  swiper:any;
+  @ViewChild('mySlider')  slides: IonSlides;
+  slidesOptions = { initialSlide: 0 }
+
+  //Variables
+  user_type = Boolean; //Si es True sera Cliente, False Empresa
+
+  format_user_form = SingupConstants.CLIENTE;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  set_user_type(type){
+    /*
+    Funcion para definir el tipo de usuario a crear 
+
+    @type: Boolean
+      - True: Usuario Cliente
+      - False: Usuario Empresa
+    */
+    this.user_type = type;
+    this.type_form();
+    this.swipeNext();
+  }
+
+  type_form(){
+    /*
+    Funcion para definir las variables del formulario del acuerdo al tipo 
+    de usuario seleccionado
+    */
+    if(this.user_type){
+      this.format_user_form = SingupConstants.CLIENTE;
+      return true;
+    }
+    else{
+      this.format_user_form = SingupConstants.EMPRESA;
+      return false;
+    }
+  }
+
+
+
+
+  // Slider block and moves events
+
+  onIonDrag(event){
+    this.swiper = event;
+    this.slides.lockSwipes(true);
+  }
+
+  swipeNext(){
+    if(this.swiper){
+      this.slides.lockSwipes(false);
+    }
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
+
+  swipeBefore(){
+    if(this.swiper){
+      this.slides.lockSwipes(false);
+    }
+    this.slides.slidePrev();
+    this.slides.lockSwipes(true);
   }
 
 }
